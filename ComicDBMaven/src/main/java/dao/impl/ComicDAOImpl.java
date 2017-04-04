37a95215b3a6015c9ce3ec13cc6c6d15a1c4ef8c
BoomannZ <s.bushuy@gmail.com>
2017-04-04 18:19:07 +0300

    ComicDAO-several methods implementation is added

--- ComicDBMaven/src/main/java/dao/impl/ComicDAOImpl.java
+++ ComicDBMaven/src/main/java/dao/impl/ComicDAOImpl.java
@@ -1,6 +1,7 @@
 package dao.impl;
 
 import dao.api.ComicDAO;
+import datasource.DataSource;
 import entity.Comic;
 import entity.ComicType;
 import entity.Status;
@@ -8,6 +9,7 @@ import holder.PropertyHolder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.sql.ConnectionPoolDataSource;
 import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.ArrayList;
@@ -29,7 +31,7 @@ public class ComicDAOImpl implements ComicDAO{
         List<ComicType> comicTypes = new ArrayList<ComicType>();
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
-            Comic comic = new Comic();
+            /* Comic comic = new Comic();
             while (resultSet.next()) {
                 comic.setId(resultSet.getInt("id"));
                 comic.setName(resultSet.getString("name"));
@@ -39,13 +41,32 @@ public class ComicDAOImpl implements ComicDAO{
             }
             comic.setComicTypeList(comicTypes);
 
-        connection.close();
+
         return comic;
+        */
+        return createComicFromResultSet(resultSet);
+
     }
 
     @Override
-    public Comic findByName(String name) {
-        return null;
+    public Comic findByName(String name) throws SQLException{
+
+        Connection connection = DataSource.getInstance().getConnection();
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM comics C JOIN comic_types CT ON C.id = CT.id_comic  where name = ?");
+        statement.setString(1, name);
+        ResultSet resultSet = statement.executeQuery();
+        /* Comic comic = new Comic();
+        List<ComicType> comicTypes = new ArrayList<ComicType>();
+        while (resultSet.next()) {
+            comic.setId(resultSet.getInt("id"));
+            comic.setName(resultSet.getString("name"));
+            comic.setDescription(resultSet.getString("description"));
+            comic.setStatus(Status.values()[resultSet.getInt("status")]);
+            comicTypes.add(ComicType.values()[resultSet.getInt("type")]);
+        }
+        comic.setComicTypeList(comicTypes);
+        */
+        return createComicFromResultSet(resultSet);
     }
 
     @Override
@@ -59,12 +80,44 @@ public class ComicDAOImpl implements ComicDAO{
     }
 
     @Override
-    public boolean update(Comic comic) {
-        return false;
+    public boolean update(Comic comic) throws SQLException {
+
+        Connection connection = DataSource.getInstance().getConnection();
+        PreparedStatement statement = connection.prepareStatement("UPDATE comics SET id = ?, name = ?, description = ?, status = ?");
+        statement.setInt(1, comic.getId());
+        statement.setString(2, comic.getName());
+        statement.setString(3, comic.getDescription());
+        statement.setInt(4, comic.getStatus().ordinal());
+        for (ComicType ct : comic.getComicTypeList()) {
+
+        }
+        boolean executionResult = statement.execute();
+        connection.close();
+        return executionResult;
     }
 
     @Override
-    public boolean delete(Comic comic) {
-        return false;
+    public boolean delete(Comic comic) throws SQLException {
+
+        Connection connection = DataSource.getInstance().getConnection();
+        PreparedStatement statement = connection.prepareStatement("DELETE * FROM comics where id = ?");
+        statement.setInt(1, comic.getId());
+        boolean executionResult = statement.execute();
+        connection.close();
+        return executionResult;
+    }
+
+    private Comic createComicFromResultSet(ResultSet resultSet) throws SQLException{
+        Comic comic = new Comic();
+        List<ComicType> comicTypes = new ArrayList<ComicType>();
+        while (resultSet.next()) {
+            comic.setId(resultSet.getInt("id"));
+            comic.setName(resultSet.getString("name"));
+            comic.setDescription(resultSet.getString("description"));
+            comic.setStatus(Status.values()[resultSet.getInt("status")]);
+            comicTypes.add(ComicType.values()[resultSet.getInt("type")]);
+        }
+        comic.setComicTypeList(comicTypes);
+        return comic;
     }
 }
